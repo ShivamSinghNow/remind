caffe171031176fba4a24fa7f37800eda285ad29
Shivam Singh <shivamsinghnow@gmail.com>
2020-03-16 23:02:11 -0700

    ss

--- forms.php
+++ forms.php
@@ -1,28 +1,48 @@
+<?php 
+$errors = '';
+$myemail = 'shivamsinghnow@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-thank-you.html');
+} 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email']; 
-    $number = $_POST['tel'];
-    $message = $_POST['message'];
-
-    $email_from = 'shivamsinghnow@gmail.com';
-
-    $email_subject = "New Form Submission";
-
-    $email_body = "User Name: $name.\n". 
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
-
-
-    $to = "shivamsinghnow@gmail.com";
-
-    $headers = "From $email_from \r\n"; 
-
-    $headers .= "Reply-To: $visitor_email\r\n"; 
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: index.html"); 
-
-
-    ?>
+echo nl2br($errors);
+?
 
